b97ad31f5d25a3c7808cd1012b924966fcf512bc
Luiz Freitas <lepd@discente.ifpe.edu.br>
2023-05-31 21:07:12 -0300

    ajustes finais

--- src/main/java/br/ifpe/triunfopixel/bean/ConsoleBean.java
+++ src/main/java/br/ifpe/triunfopixel/bean/ConsoleBean.java
@@ -1,14 +1,9 @@
 package br.ifpe.triunfopixel.bean;
 
 import br.ifpe.triunfopixel.model.Console;
-import br.ifpe.triunfopixel.model.Game;
 import br.ifpe.triunfopixel.service.ConsoleService;
-import br.ifpe.triunfopixel.service.GameService;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -16,11 +11,6 @@ import javax.faces.bean.ManagedBean;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
-import org.primefaces.model.DefaultStreamedContent;
-import org.primefaces.model.StreamedContent;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
